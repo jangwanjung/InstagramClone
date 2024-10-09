0c58c905e7f71b573f1ab8edf9e9c179eaf73f70
jangwanjung <tjdrj530@naver.com>
2024-10-09 20:36:16 +0900

    되돌리기

--- src/main/java/project/instagramclone/web/ImageController.java
+++ src/main/java/project/instagramclone/web/ImageController.java
@@ -11,6 +11,7 @@ public class ImageController {
     @GetMapping({"","/","/image/feed"})
     public String feed(@AuthenticationPrincipal PrincipalDetails principal) {
         System.out.println(principal.getUsername());
+        a
         return "image/feed";
     }
 }
