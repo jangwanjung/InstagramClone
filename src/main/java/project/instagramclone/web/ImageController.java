676738421bde618e67184a30d1f10f792c975387
jangwanjung <tjdrj530@naver.com>
2024-10-09 18:53:49 +0900

    나의 세션 오브젝트 만들기 완료

--- src/main/java/project/instagramclone/web/ImageController.java
+++ src/main/java/project/instagramclone/web/ImageController.java
@@ -1,16 +1,20 @@
 package project.instagramclone.web;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import project.instagramclone.config.auth.PrincipalDetails;
+import project.instagramclone.dto.LoginUser;
 
 @Controller
 public class ImageController {
 
     @GetMapping({"","/","/image/feed"})
-    public String feed(@AuthenticationPrincipal PrincipalDetails principal) {
+    public String feed(@AuthenticationPrincipal PrincipalDetails principal, HttpSession session) {
         System.out.println(principal.getUsername());
+        LoginUser loginUser = (LoginUser) session.getAttribute("loginUser");
+        System.out.println(loginUser);
         return "image/feed";
     }
 }
