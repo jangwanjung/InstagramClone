de0c525cdee30db299d23ca69b0be6bcdf5071c5
jangwanjung <tjdrj530@naver.com>
2024-10-09 17:21:18 +0900

    feed페이지 완료

---
+++ src/main/java/project/instagramclone/web/ImageController.java
@@ -0,0 +1,16 @@
+package project.instagramclone.web;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import project.instagramclone.config.auth.PrincipalDetails;
+
+@Controller
+public class ImageController {
+
+    @GetMapping({"","/","/image/feed"})
+    public String feed(@AuthenticationPrincipal PrincipalDetails principal) {
+        System.out.println(principal.getUsername());
+        return "image/feed";
+    }
+}
